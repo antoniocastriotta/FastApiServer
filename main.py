8671cd8834bd3f0730b3716972ce99eea95213d1
antoniocastriotta <134046725+antoniocastriotta@users.noreply.github.com>
2023-11-30 16:25:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ from typing import List
 import logging
 import random
 
-# Configura l'URL del tuo database AWS RDS
-DATABASE_URL = "mysql://admin:P47#$53PNde@mydb.czqucuuf76q6.eu-central-1.rds.amazonaws.com/Pazientidatabase"
-
 # Inizializza l'app FastAPI
 app = FastAPI()
+
+# Configura il logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# Configura l'URL del database 
+DATABASE_URL = "mysql://admin:P47#$53PNde@mydb.czqucuuf76q6.eu-central-1.rds.amazonaws.com/Pazientidatabase"  # URL di esempio
 
 # Configurazione del motore del database
 engine = create_engine(DATABASE_URL)
@@ -22,20 +26,19 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Dichiarazione della base per i modelli SQLAlchemy
 Base = declarative_base()
 
-# Configura il logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
 # Definizione del modello per la tabella dei pazienti
 class Paziente(Base):
     __tablename__ = "pazienti"
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(50), index=True)
     cognome = Column(String(50), index=True)
-    data_nascita = Column(String(20))  # Imposta la lunghezza a 20, ad esempio
-    codice_fiscale = Column(String(16), unique=True)  # Imposta la lunghezza a 16
-    patologia = Column(String(100))  # Imposta la lunghezza a 100, ad esempio
-    sesso = Column(String(10))  # Imposta la lunghezza a 10, ad esempio
+    data_nascita = Column(String(20))
+    codice_fiscale = Column(String(16), unique=True)
+    patologia = Column(String(100))
+    sesso = Column(String(10))
+    id_medico = Column(Integer, ForeignKey("medici.id"))
+
+    medico = relationship("Medico", back_populates="pazienti")
 
     acquisizioni = relationship("Acquisizione", back_populates="paziente")
 
@@ -44,11 +47,20 @@ class Acquisizione(Base):
     __tablename__ = "acquisizioni"
     acquisizione_id = Column(Integer, primary_key=True, index=True)
     id_paziente = Column(Integer, ForeignKey("pazienti.id"))
-    hb_value = Column(String(50))  # Imposta la lunghezza a 50, ad esempio
-    acquisition_date = Column(String(20))  # Imposta la lunghezza a 20, ad esempio
-    acquisition_uri = Column(String(255))  # Imposta la lunghezza a 255, ad esempio
+    hb_value = Column(String(50))
+    acquisition_date = Column(String(20))
+    acquisition_uri = Column(String(255))
 
     paziente = relationship("Paziente", back_populates="acquisizioni")
+
+# Definizione del modello per la tabella dei medici
+class Medico(Base):
+    __tablename__ = "medici"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    password = Column(String(255))
+
+    pazienti = relationship("Paziente", back_populates="medico")
 
 # Crea le tabelle nel database
 Base.metadata.create_all(bind=engine)
@@ -85,6 +97,11 @@ class AcquisizioneDtoWithId(AcquisizioneDto):
     acquisition_date: str
     acquisition_uri: str
 
+# Modello Pydantic per il DTO del Medico
+class MedicoDto(BaseModel):
+    username: str
+    password: str
+
 # Funzione di dipendenza per ottenere la sessione del database
 def get_db():
     db = SessionLocal()
@@ -94,15 +111,33 @@ def get_db():
         db.close()
 
 # Implementazione delle richieste
-@app.post("/salva_paziente")
-def salva_paziente(pazienteDto: PazienteDto, db: Session = Depends(get_db)):
-    # Effettua la logica per salvare il paziente nel database
-    nuovo_paziente = Paziente(**pazienteDto.dict())
+@app.post("/salva_paziente/{medicoId}")
+def salva_paziente(medicoId: int, pazienteDto: PazienteDto, db: Session = Depends(get_db)):
+    # Effettua la logica per salvare il paziente nel database con l'ID del medico
+    nuovo_paziente = Paziente(id_medico=medicoId, **pazienteDto.dict())
     db.add(nuovo_paziente)
     db.commit()
     db.refresh(nuovo_paziente)
 
     return {"message": "Paziente salvato con successo"}
+
+@app.get("/get_pazienti_by_medico/{medicoId}", response_model=List[PazienteDtoWithId])
+def get_pazienti_by_medico(medicoId: int, db: Session = Depends(get_db)):
+    pazienti = db.query(Paziente).filter(Paziente.id_medico == medicoId).all()
+
+    pazienti_dto = [
+        PazienteDtoWithId(
+            id=paziente.id,
+            nome=paziente.nome,
+            cognome=paziente.cognome,
+            data_nascita=paziente.data_nascita,
+            codice_fiscale=paziente.codice_fiscale,
+            patologia=paziente.patologia,
+            sesso=paziente.sesso
+        ) for paziente in pazienti
+    ]
+
+    return pazienti_dto
 
 # Implementazione della richiesta di salvataggio di un'acquisizione
 @app.post("/salva_acquisizione/{pazienteId}")
@@ -112,7 +147,7 @@ def salva_acquisizione(pazienteId: int, acquisizioneDto: AcquisizioneDto, db: Se
     if not paziente:
         raise HTTPException(status_code=404, detail="Paziente non trovato")
 
-    # Effettua la logica per salvare l'acquisizione nel database
+    # Logica per salvare l'acquisizione nel database
     nuova_acquisizione = Acquisizione(id_paziente=pazienteId, **acquisizioneDto.dict())
     db.add(nuova_acquisizione)
     db.commit()
@@ -204,26 +239,6 @@ def delete_paziente(pazienteId: int, db: Session = Depends(get_db)):
 
     return {"message": "Paziente e relative acquisizioni eliminate con successo"}
 
-# Implementazione della richiesta per ottenere tutti i pazienti
-@app.get("/get_pazienti", response_model=List[PazienteDtoWithId])
-def get_pazienti(db: Session = Depends(get_db)):
-    pazienti = db.query(Paziente).all()
-    
-    # Converte gli oggetti Paziente in oggetti PazienteDtoWithId
-    pazienti_dto = [
-        PazienteDtoWithId(
-            id=paziente.id,
-            nome=paziente.nome,
-            cognome=paziente.cognome,
-            data_nascita=paziente.data_nascita,
-            codice_fiscale=paziente.codice_fiscale,
-            patologia=paziente.patologia,
-            sesso=paziente.sesso
-        ) for paziente in pazienti
-    ]
-
-    return pazienti_dto
-
 # Implementazione della richiesta per ottenere il valore di hb
 @app.get("/get_hb_value")
 def get_hb_value():
@@ -244,3 +259,34 @@ def get_total_acquisizioni(pazienteId: int, db: Session = Depends(get_db)):
     total_acquisizioni = db.query(Acquisizione).filter(Acquisizione.id_paziente == pazienteId).count()
 
     return total_acquisizioni
+
+# Implementazione della richiesta di registrazione per il medico
+@app.post("/registra_medico")
+def registra_medico(medicoDto: MedicoDto, db: Session = Depends(get_db)):
+    # Verifica se esiste già un medico con lo stesso username
+    existing_medico = db.query(Medico).filter(Medico.username == medicoDto.username).first()
+    if existing_medico:
+        raise HTTPException(status_code=400, detail="Username già in uso")
+
+    # Crea un nuovo medico nel database
+    nuovo_medico = Medico(**medicoDto.dict())
+    db.add(nuovo_medico)
+    db.commit()
+    db.refresh(nuovo_medico)
+
+    return {"message": "Medico registrato con successo"}
+
+# Implementazione della richiesta di login per il medico
+@app.post("/login_medico")
+def login_medico(medicoDto: MedicoDto, db: Session = Depends(get_db)):
+    # Cerca il medico nel database
+    medico = db.query(Medico).filter(
+        Medico.username == medicoDto.username,
+        Medico.password == medicoDto.password
+    ).first()
+
+    if not medico:
+        raise HTTPException(status_code=401, detail="Credenziali non valide")
+
+    # Ritorna l'ID del medico
+    return medico.id
